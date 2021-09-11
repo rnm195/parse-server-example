3c739852f8634dd265531bb6c331f50af6e8ff3d
rnm195 <90164842+rnm195@users.noreply.github.com>
2021-09-11 23:11:57 +1000

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const args = process.argv || [];
 const test = args.some(arg => arg.includes('jasmine'));
 
-const databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+const databaseUri = process.env.DATABASE_URL || process.env.MONGODB_URI;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
